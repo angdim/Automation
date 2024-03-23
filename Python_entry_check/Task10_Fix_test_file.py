ac330dd12045969c9ccef0a5b6fdc4abf7ffaa62
Angel Dimitrov <angel.dimitrov@strypes.eu>
2024-03-23 15:22:47 +0200

    Tasks update

--- Python_entry_check/Task10_Fix_test_file.py
+++ Python_entry_check/Task10_Fix_test_file.py
@@ -1,3 +1,16 @@
+
 def xml_automation(test):
     with open(test, "r") as f:
         temp_copy = f.read().split("\n")
+        with open("test-automated.xml", "w") as new_file:
+            for current_line in temp_copy:
+                if temp_copy.index(current_line)+1 < len(temp_copy):
+                    if "<subclass>Recovery</subclass>" in temp_copy[temp_copy.index(current_line)+1]:
+                        new_line = current_line.replace('<description>', '<description>(Automated) ')
+                        new_file.write(new_line + "\n")
+                    else:
+                        new_file.write(current_line + "\n")
+                else:
+                    new_file.write(current_line)
+
+xml_automation("test-suite.xml")
--- Python_entry_check/Task7_Student_management_system.py
+++ Python_entry_check/Task7_Student_management_system.py
@@ -1,3 +1,47 @@
 class Student:
-    def __init__(self, first_name, last_name, student_id, age) -> None:
-        (pass)
+    def __init__(self, first_name:str, last_name:str, student_id:int, age:str) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.student_id = student_id
+        self.age = age
+
+class StudentManagement(Student):
+    def __init__(self):
+        self.Students = []
+
+    def accept(self, student:Student):
+        for st in self.Students:
+            if st.student_id == student.student_id:
+                print(f'Student with ID {st.student_id} already exists')
+            else:
+                self.Students.append(student)
+
+    def display(self, id):
+        st = self.search(id)
+        if st:
+            print(f'{st.first_name} {st.last_name} is {st.age}-years old and has an ID of {st.student_id}.')
+
+    def search(self, id):
+         for st in self.Students:
+            if st.student_id == id:
+                 return st
+            else:
+                 print(f'Student with ID {id} does not exist.')
+
+    def delete(self, id):
+        st = self.search(id)
+        if st:
+            self.Students.remove(st)
+
+student1 = Student("John", "Doe", 1, 18)
+student2 = Student("Peter", "Oswell", 1, 15)
+student3 = Student("Lilliam", "Pumpernickle", 2, 22)
+
+student_management = StudentManagement()
+student_management.accept(student1)
+student_management.accept(student2)
+student_management.accept(student3)
+student_management.display(3)
+student_management.display(2)
+student_management.delete(1)
+student_management.display(1)
