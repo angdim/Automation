eb1369cc9e9a97413d83dc5a1715a7b5b4edb188
Angel Dimitrov <angel.dimitrov@strypes.eu>
2024-03-24 15:02:56 +0200

    List operations task, improved

--- Python_entry_check/Task3_List_operations.py
+++ Python_entry_check/Task3_List_operations.py
@@ -1,6 +1,4 @@
 evens = [x for x in range(1, 21) if x%2 == 0]
 no_5s = [y for y in evens if not y%5 == 0]
-print('Original list: ', end='')
-print(*evens, sep=', ')
-print('Modified list: ', end='')
-print(*no_5s, sep=', ')
+print('Original list: ', *evens, sep=', ')
+print('Modified list: ', *no_5s, sep=', ')
